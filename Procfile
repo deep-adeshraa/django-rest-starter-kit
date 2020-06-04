71ff793c5ea61acce2c52804499f19bee8ea07f6
deepadeshraa <deep@digiqt.com>
2020-06-04 14:19:53 +0530

    heroku config added

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn ThrottleLabs.wsgi --log-file -
+
--- ThrottleLabs/settings.py
+++ ThrottleLabs/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '9+#_q#t9qr=ep=)bjp&pw^#6*g!-u1zvbr-sj(#w)(fl02h80&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com', 'localhost']
 
 # Application definition
 
@@ -46,6 +46,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,3 +123,15 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+import dj_database_url
+
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
